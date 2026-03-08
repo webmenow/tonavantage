c2486c9619937c229baeb104e75631b81a311f01
webmenow <44297473+webmenow@users.noreply.github.com>
2026-03-08 17:11:39 +0100

    Update eleventy.config.cjs

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,3 +1,13 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("assets");
+
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("_posts/*.md");
+  });
+
+  // Исправляем URL постов: /_posts/... -> /posts/...
+  eleventyConfig.addFilter("postUrl", function(url) {
+    return url.replace(/^\/_posts\//, "/posts/");
+  });
 };
